7cc3e8ed26524fc276b2ffa9a3e6ffebae774460
mlata <mlata@dd>
2023-04-15 11:19:45 +0200

    add meetings endpoint

---
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,27 @@
+package com.company.enroller.controllers;
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.persistence.MeetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController {
+
+    @Autowired
+    MeetingService meetingsService;
+
+    @GetMapping("")
+    public ResponseEntity<?> getMeetings(){
+
+        Collection<Meeting> meetings = meetingsService.getAll();
+        return  new ResponseEntity<>(meetings, HttpStatus.OK);
+    }
+}
