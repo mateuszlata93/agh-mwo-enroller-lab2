ca0db713cc213721dd635ebf96c2234fcff8eba6
mlata <mlata@dd>
2023-04-15 11:33:05 +0200

    add getMeeting

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +21,16 @@ public class MeetingRestController {
 
     @GetMapping("")
     public ResponseEntity<?> getMeetings(){
-
         Collection<Meeting> meetings = meetingsService.getAll();
         return  new ResponseEntity<>(meetings, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> getMeeting(@PathVariable("id") long id){
+        Meeting meeting = meetingsService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(meeting, HttpStatus.OK);
     }
 }
